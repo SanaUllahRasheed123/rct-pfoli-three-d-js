53a0e0abd607fadf823944570ba7d03e63efe70e
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-11-26 14:58:36 +0500

    workingOnIt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,13 +16,14 @@ const Home = () => {
 <Canvas className='w-full h-screen bg-transparent' 
 camera={{near:0.1,far:1000}}
 >
+<Island/>
+
 <Suspense fallback={<Loader/>}>
 <directionalLight/>
 <ambientLight/>
 <pointLight/>
 <spotLight/>
 <hemisphereLight/>
-<Island/>
 </Suspense>
 </Canvas>
 </section>
